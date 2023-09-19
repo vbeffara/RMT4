813959fcad2c2b8d049ed591a9aa88697bf518b9
Vincent Beffara <vbeffara@gmail.com>
2023-09-19 10:06:06 +0200

    We do not need a subdivision to be increasing

--- RMT4/pintegral.lean
+++ RMT4/pintegral.lean
@@ -3,7 +3,7 @@ import Mathlib.Analysis.Calculus.ContDiffDef
 import Mathlib.Analysis.Calculus.Deriv.Basic
 import Mathlib.MeasureTheory.Integral.IntervalIntegral
 
-open List BigOperators Metric Set
+open Finset BigOperators Metric Set
 
 lemma exists_div_lt {a ε : ℝ} (ha : 0 < a) (hε : 0 < ε) : ∃ n : ℕ, a / (n + 1) < ε := by
   obtain ⟨n, hn⟩ := exists_nat_one_div_lt (div_pos hε ha)
@@ -12,52 +12,40 @@ lemma exists_div_lt {a ε : ℝ} (ha : 0 < a) (hε : 0 < ε) : ∃ n : ℕ, a / 
 
 structure subdivision (a b : ℝ) where
   n : ℕ
-  toFun : Fin (n + 2) → ℝ
-  mono : Monotone toFun
-  first : toFun ⟨0, Nat.succ_pos (n+1)⟩ = a
-  last : toFun (Fin.last (n+1)) = b
+  toFun : ℕ → ℝ
+  first : toFun 0 = a
+  last : toFun (n + 1) = b
+  subset : ∀ {i}, i ≤ n + 1 → toFun i ∈ Set.Icc a b
 
 namespace subdivision
 
 variable {a b : ℝ} {σ : subdivision a b}
 
-instance : CoeFun (subdivision a b) (λ σ => Fin (σ.n + 2) → ℝ) := ⟨toFun⟩
+instance : CoeFun (subdivision a b) (λ _ => ℕ → ℝ) := ⟨toFun⟩
 
-def zero (σ : subdivision a b) : Fin (σ.n + 2) := ⟨0, Nat.succ_pos (σ.n + 1)⟩
+def Icc (σ : subdivision a b) (i : Fin (σ.n + 1)) : Set ℝ := Set.Icc (σ i) (σ i.succ)
 
-def Icc (σ : subdivision a b) (i : Fin (σ.n + 1)) : Set ℝ := Set.Icc (σ i.castSucc) (σ i.succ)
+noncomputable def mesh (σ : subdivision a b) : ℝ := ⨆ i : Fin (σ.n + 1), |σ i.succ - σ i|
 
-noncomputable def mesh (σ : subdivision a b) : ℝ :=
-  ⨆ i : Fin (σ.n + 1), |σ i.succ - σ i.castSucc|
+lemma le_mesh {i : Fin (σ.n + 1)} : |σ (i + 1) - σ i| ≤ σ.mesh :=
+  le_ciSup (f := λ i : Fin (σ.n + 1) => |σ i.succ - σ i|) (finite_range _).bddAbove _
 
-lemma le_mesh {i : Fin (σ.n + 1)} : |σ i.succ - σ i.castSucc| ≤ σ.mesh :=
-  le_ciSup (f := λ i => |σ (Fin.succ i) - σ (Fin.castSucc i)|) (finite_range _).bddAbove _
-
-lemma le (σ : subdivision a b) : a ≤ b := by
-  convert ← σ.mono (zero σ).le_last
-  · exact σ.first
-  · exact σ.last
-
-lemma subset {σ : subdivision a b} {i} : σ i ∈ Set.Icc a b := by
-  refine ⟨?_, ?_⟩
-  · simp only [← σ.first]
-    apply σ.mono
-    simp only [Fin.mk_zero, Fin.zero_le]
-  · simp only [← σ.last]
-    apply σ.mono
-    apply Fin.le_last
+lemma le (σ : subdivision a b) : a ≤ b :=
+  (σ.first ▸ σ.subset (zero_le (σ.n + 1))).2
 
 noncomputable def regular (hab : a ≤ b) (n : ℕ) : subdivision a b where
   n := n
   toFun := λ i => a + i * (b - a) / (n + 1)
-  mono := by
-    intro i j ij
-    dsimp only
-    gcongr
-    · linarith
-    · norm_cast
   first := by simp
   last := by field_simp; ring
+  subset := by
+    have h0 : 0 ≤ b - a := by linarith
+    intro i hi
+    have h3 : (i : ℝ) ≤ n + 1 := by norm_cast
+    have h4 : b = a + (n + 1) * (b - a) / (n + 1) := by field_simp; ring
+    constructor
+    · simp; positivity
+    · nth_rewrite 2 [h4]; simp; gcongr
 
 @[simp] lemma regular_mesh (hab : a < b) {n : ℕ} :
     (regular hab.le n).mesh = (b - a) / (n + 1) := by
@@ -78,9 +66,12 @@ lemma adapted_of_mesh_lt (h1 : ∀ i, IsOpen (S i)) (h2 : Set.Icc a b ⊆ ⋃ i,
     ∃ ε > 0, ∀ σ : subdivision a b, σ.mesh < ε → adapted σ S := by
   obtain ⟨ε, hε, l1⟩ := lebesgue_number_lemma_of_metric isCompact_Icc h1 h2
   refine ⟨ε, hε, λ σ hσ j => ?_⟩
-  obtain ⟨i, hi⟩ := l1 (σ j.castSucc) σ.subset
+  obtain ⟨i, hi⟩ := l1 (σ j.castSucc) (σ.subset j.prop.le)
   have : Set.OrdConnected (ball (σ j.castSucc) ε) := (convex_ball ..).ordConnected
-  exact ⟨i, subset_trans (Set.Icc_subset _ (mem_ball_self hε) (le_mesh.trans_lt hσ)) hi⟩
+  use i
+  refine subset_trans ?_ hi
+  refine Set.Icc_subset _ (mem_ball_self hε) ?_
+  exact le_mesh.trans_lt hσ
 
 lemma exists_adapted (hab : a < b) (h1 : ∀ i, IsOpen (S i)) (h2 : Set.Icc a b ⊆ ⋃ i, S i) :
     ∃ σ : subdivision a b, adapted σ S := by
@@ -89,10 +80,13 @@ lemma exists_adapted (hab : a < b) (h1 : ∀ i, IsOpen (S i)) (h2 : Set.Icc a b 
   have : (regular hab.le n).mesh = (b - a) / (n + 1) := regular_mesh hab
   exact ⟨regular hab.le n, h _ (by linarith)⟩
 
+def sum (σ : subdivision a b) (f : ℕ → ℝ → ℝ → ℂ) : ℂ :=
+  ∑ i in range (σ.n + 1), f i (σ i) (σ (i + 1))
+
 end subdivision
 
 noncomputable def sumSub (σ : subdivision a b) (F : Fin (σ.n + 1) -> ℝ -> ℂ) : ℂ :=
-  ∑ i, (F i (σ i.succ) - F i (σ i.castSucc))
+  σ.sum (λ i x y => F i y - F i x)
 
 noncomputable def sumSubAlong (σ : subdivision a b) (F : Fin (σ.n + 1) → ℂ → ℂ)
     (γ : ℝ → ℂ) : ℂ :=
@@ -114,7 +108,7 @@ noncomputable def pintegral (hab : a < b) (f : ℂ → ℂ) (γ : ℝ → ℂ) (
   exact sumSubAlong σ (λ i => F (γ (I i))) γ
 
 def isPiecewiseDiffAlong (γ : ℝ → ℂ) (σ : subdivision a b) : Prop :=
-  ∀ i : Fin (σ.n + 1), ContDiffOn ℝ 1 γ (σ.Icc i)
+  ∀ i < σ.n + 1, ContDiffOn ℝ 1 γ (σ.Icc i)
 
 noncomputable def piecewiseIntegral (F : ℂ → ℂ) (γ : ℝ → ℂ) (σ : subdivision a b) : ℂ :=
   ∑ i : Fin (σ.n + 1), (∫ t in (σ i.castSucc)..(σ i.succ), F (γ t) * deriv γ t)
@@ -122,14 +116,6 @@ noncomputable def piecewiseIntegral (F : ℂ → ℂ) (γ : ℝ → ℂ) (σ : s
 lemma isLocDerivOn_deriv : IsLocDerivOn U (deriv F) := by
   intro z _; exact ⟨1, zero_lt_one, F, eqOn_refl _ _⟩
 
-lemma preTelescopic {f : Fin (n + 1) → ℝ} :
-    ∑ i : Fin n, (f i.succ - f i.castSucc) = f (Fin.last n) - f 0 := by
-  induction n with
-  | zero => simp [Fin.last]
-  | succ n ih =>
-    simp
-    sorry
-
-lemma telescopic : sumSub σ (λ i => f) = f b - f a := by
-  simp [sumSub, ← σ.first]
-  sorry
+lemma telescopic {σ : subdivision a b} : sumSub σ (λ _ => f) = f b - f a := by
+  simp only [sumSub, ← σ.first, ← σ.last]
+  apply Finset.sum_range_sub (f := f ∘ σ)
