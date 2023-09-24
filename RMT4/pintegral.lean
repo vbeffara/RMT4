7036c1a18a3b2580520e132559a223931e4e47fe
Vincent Beffara <vbeffara@gmail.com>
2023-09-25 00:39:44 +0200

    Integral of 0 is 0

--- RMT4/pintegral.lean
+++ RMT4/pintegral.lean
@@ -9,23 +9,23 @@ import RMT4.Subdivision
 open BigOperators Metric Set Subdivision Topology Filter
 
 structure IsLocDerivOn (U : Set ℂ) (f : ℂ → ℂ) :=
-  F : ℂ → ℂ → ℂ
-  S : ℂ → Set ℂ
-  mem {z : ℂ} (hz : z ∈ U) : z ∈ S z
-  opn {z : ℂ} (hz : z ∈ U) : IsOpen (S z)
-  sub {z : ℂ} (hz : z ∈ U) : S z ⊆ U
-  dif {z : ℂ} (hz : z ∈ U) : DifferentiableOn ℂ (F z) (S z)
-  eqd {z : ℂ} (hz : z ∈ U) : (S z).EqOn f (deriv (F z))
+  F : U → ℂ → ℂ
+  S : U → Set ℂ
+  mem (z : U) : z.1 ∈ S z
+  opn (z : U) : IsOpen (S z)
+  sub (z : U) : S z ⊆ U
+  dif (z : U) : DifferentiableOn ℂ (F z) (S z)
+  eqd (z : U) : (S z).EqOn f (deriv (F z))
 
-lemma IsLocDerivOn.nhd (h : IsLocDerivOn U f) (hz : z ∈ U) : h.S z ∈ 𝓝 z :=
-  (h.opn hz).mem_nhds (h.mem hz)
+lemma IsLocDerivOn.nhd (h : IsLocDerivOn U f) (z : U) : h.S z ∈ 𝓝 z.1 :=
+  (h.opn z).mem_nhds (h.mem z)
 
 noncomputable def isLocDerivOn_deriv (hU : IsOpen U) (hF : DifferentiableOn ℂ F U) :
     IsLocDerivOn U (deriv F) where
   F _ := F
   S _ := U
   sub _ := by rfl
-  mem hz := hz
+  mem z := z.2
   opn _ := hU
   eqd _ := eqOn_refl (deriv F) U
   dif _ := hF
@@ -34,7 +34,9 @@ section pintegral
 
 noncomputable def pintegral (hab : a ≤ b) (f : ℂ → ℂ) (γ : ℝ → ℂ) (h2 : MapsTo γ (Set.Icc a b) U)
     (hγ : ContinuousOn γ (Set.Icc a b)) (hf : IsLocDerivOn U f) : ℂ :=
-  have h1 (t : Set.Icc a b) : ∃ i, hf.S i ∈ 𝓝 (γ t) := ⟨γ t, hf.nhd (h2 t.2)⟩
+  have h1 (t : Set.Icc a b) : ∃ i, hf.S i ∈ 𝓝 (γ t) :=
+    let u : U := ⟨γ t, h2 t.2⟩
+    ⟨u, hf.nhd u⟩
   let ⟨σ, hσ⟩ := exists_reladapted hab hγ h1
   let RW := hσ.witness
   σ.sumSubAlong (hf.F ∘ RW.I) γ
@@ -61,19 +63,24 @@ lemma apply_eq_of_path (hab : a ≤ b) (f : ℂ → ℂ) (hf : IsLocallyConstant
   exact @IsLocallyConstant.apply_eq_of_isPreconnected _ _ _ _ (h2) _ isPreconnected_univ
     ⟨b, hab, le_rfl⟩ ⟨a, le_rfl, hab⟩ (mem_univ _) (mem_univ _)
 
-example {σ : Subdivision a b} {DW : IsLocDerivOn U f} {RW RW' : reladapted_witness σ DW.S γ} :
-    σ.sumSubAlong (DW.F ∘ RW.I) γ = σ.sumSubAlong (DW.F ∘ RW'.I) γ := by
+lemma sumSubAlong_eq_zero {σ : Subdivision a b} {DW : IsLocDerivOn U 0}
+  {RW : reladapted_witness σ DW.S γ} (hγ : ContinuousOn γ (Set.Icc a b)) :
+    σ.sumSubAlong (DW.F ∘ RW.I) γ = 0 := by
   simp only [sumSubAlong, sumSub, sum]
-  apply Finset.sum_congr rfl
+  apply Finset.sum_eq_zero
   intro k hk
-  set φ := (DW.F ∘ RW.I) k
-  set ψ := (DW.F ∘ RW'.I) k
-  set F := φ - ψ
-  set x := γ (σ k)
-  set y := γ (σ (k + 1))
-  rw [sub_eq_sub_iff_sub_eq_sub]
-  change F y = F x
-  have h1 := RW.hI k
-  have h2 := RW'.hI k
-  have h3 := subset_inter h1 h2
-  sorry
+  rw [Finset.mem_range] at hk
+  rw [sub_eq_zero]
+  apply apply_eq_of_path (U := DW.S (RW.I k))
+  · refine σ.mono hk.le ?_ (Nat.le_succ k)
+    simpa only [mem_Iic, add_le_add_iff_right] using Nat.lt_succ.1 hk
+  · apply isLocallyConstant_of_deriv_eq_zero (DW.opn _) _ (DW.dif _)
+    exact λ _ hz => (DW.eqd (RW.I k) hz).symm
+  · apply hγ.mono
+    convert σ.Icc_subset (i := k)
+    simp only [Subdivision.Icc, Fin.coe_ofNat_eq_mod, Fin.val_succ, Nat.mod_eq_of_lt hk]
+  · apply (mapsTo'.2 (RW.hI k)).mono_left
+    simpa [Subdivision.Icc, Nat.mod_eq_of_lt hk] using subset_rfl
+
+lemma pintegral_zero : pintegral hab 0 γ h2 hγ hf = 0 := by
+  simp only [pintegral, sumSubAlong_eq_zero hγ]
