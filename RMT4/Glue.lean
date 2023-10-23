b1b910bc216f0d52301477c589836a44f0e26ecf
Vincent Beffara <vbeffara@gmail.com>
2023-10-23 13:26:42 +0200

    Cleaner

--- RMT4/Glue.lean
+++ RMT4/Glue.lean
@@ -5,8 +5,11 @@ set_option pp.proofs.withType false
 
 open Set Interval
 
-variable {a b c t : ℝ} {E : Type*} [TopologicalSpace E] {f g : ℝ → E}
-  {F : Icc a b → E} {G : Icc b c → E}
+variable {a b c t : ℝ} {E : Type*} [TopologicalSpace E]
+
+section real
+
+variable {f g : ℝ → E}
 
 noncomputable def glue_at (b : ℝ) (f g : ℝ → E) := λ t => if t ≤ b then f t else g t
 
@@ -14,12 +17,14 @@ lemma continuous_glue (hf : Continuous f) (hg : Continuous g) (h : f b = g b) :
     Continuous (glue_at b f g) :=
   hf.if_le hg continuous_id continuous_const (λ _ hx => hx.symm ▸ h)
 
+end real
+
+section Icc
+
+variable {F : Icc a b → E} {G : Icc b c → E}
+
 noncomputable def glue_Icc (F : Icc a b → E) (G : Icc b c → E) (t : Icc a c) : E :=
   if h : t ≤ b then F ⟨t, t.2.1, h⟩ else G ⟨t, not_le.1 h |>.le, t.2.2⟩
-
-open Classical in
-noncomputable def glue_uIcc (F : uIcc a b → E) (G : uIcc b c → E) (t : uIcc a c) : E :=
-  if h : ↑t ∈ uIcc a b then F ⟨t, h⟩ else G ⟨t, uIcc_subset_uIcc_union_uIcc t.2 |>.resolve_left h⟩
 
 lemma glue_Icc_eq (hab : a ≤ b) (hbc : b ≤ c) :
     glue_Icc F G = λ t : Icc a c => if t ≤ b then IccExtend hab F t else IccExtend hbc G t := by
@@ -37,5 +42,20 @@ noncomputable def ContinuousMap.trans (hab : a ≤ b) (hbc : b ≤ c) (F : C(Icc
   toFun := glue_Icc F G
   continuous_toFun := continuous_glue_Icc F.continuous G.continuous hab hbc h
 
-noncomputable def ContinuousMap.trans' (F : C(uIcc a b, E)) (G : C(uIcc b c, E))
-    (h : F ⟨_, right_mem_uIcc⟩ = G ⟨_, left_mem_uIcc⟩) : C(uIcc a c, E) := sorry
+end Icc
+
+section uIcc
+
+variable {F : Icc a b → E} {G : uIcc b c → E}
+
+open Classical
+
+noncomputable def glue_uIcc (F : Icc a b → E) (G : uIcc b c → E) (t : Icc a c) : E :=
+  if h : t ≤ b then F ⟨t, t.2.1, h⟩ else
+    G ⟨t, inf_le_left.trans <| not_le.1 h |>.le, t.2.2.trans le_sup_right⟩
+
+lemma glue_uIcc_eq (hab : a ≤ b) : glue_uIcc F G = λ t : Icc a c =>
+    if t ≤ b then IccExtend hab F t else IccExtend inf_le_sup G t := by
+  ext t ; simp [glue_uIcc] ; split_ifs <;> symm <;> apply IccExtend_of_mem
+
+end uIcc
