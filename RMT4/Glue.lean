7416247d794cabf017349a2e685654b8babece37
Vincent Beffara <vbeffara@gmail.com>
2023-10-23 17:25:04 +0200

    Finish useful gluing lemma

--- RMT4/Glue.lean
+++ RMT4/Glue.lean
@@ -48,8 +48,6 @@ section uIcc
 
 variable {F : Icc a b → E} {G : uIcc b c → E}
 
-open Classical
-
 noncomputable def glue_uIcc (F : Icc a b → E) (G : uIcc b c → E) (t : Icc a c) : E :=
   if h : t ≤ b then F ⟨t, t.2.1, h⟩ else
     G ⟨t, inf_le_left.trans <| not_le.1 h |>.le, t.2.2.trans le_sup_right⟩
@@ -57,5 +55,18 @@ noncomputable def glue_uIcc (F : Icc a b → E) (G : uIcc b c → E) (t : Icc a 
 lemma glue_uIcc_eq (hab : a ≤ b) : glue_uIcc F G = λ t : Icc a c =>
     if t ≤ b then IccExtend hab F t else IccExtend inf_le_sup G t := by
   ext t ; simp [glue_uIcc] ; split_ifs <;> symm <;> apply IccExtend_of_mem
+
+lemma continuous_glue_uIcc (hF : Continuous F) (hG : Continuous G) (hab : a ≤ b)
+    (h : F ⟨b, right_mem_Icc.2 hab⟩ = G ⟨b, left_mem_uIcc⟩) : Continuous (glue_uIcc F G) := by
+  rw [glue_uIcc_eq hab]
+  suffices Continuous fun t => if t ≤ b then IccExtend hab F t else IccExtend inf_le_sup G t from
+    this.comp continuous_subtype_val
+  refine hF.Icc_extend'.if_le hG.Icc_extend' continuous_id continuous_const (λ x hx => ?_)
+  simpa [IccExtend_of_mem, hx] using h
+
+noncomputable def ContinuousMap.trans' (hab : a ≤ b) (F : C(Icc a b, E)) (G : C(uIcc b c, E))
+    (h : F ⟨b, right_mem_Icc.2 hab⟩ = G ⟨b, left_mem_uIcc⟩) : C(Icc a c, E) where
+  toFun := glue_uIcc F G
+  continuous_toFun := continuous_glue_uIcc F.continuous G.continuous hab h
 
 end uIcc
