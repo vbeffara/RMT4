95f23febe7a97aff8f852b194ceeaec6acf92ea0
Vincent Beffara <vbeffara@gmail.com>
2024-02-25 00:45:48 +0100

    Setup blueprint

--- lakefile.lean
+++ lakefile.lean
@@ -8,8 +8,6 @@ package «rMT4» {
 
 require mathlib from git "https://github.com/leanprover-community/mathlib4.git"
 
-require «doc-gen4» from git "https://github.com/leanprover/doc-gen4" @ "main"
-
 -- require LeanCopilot from git "https://github.com/lean-dojo/LeanCopilot.git" @ "v1.0.2"
 
 @[default_target]
@@ -17,3 +15,9 @@ lean_lib RMT4 {
   -- add any library configuration options here
   -- moreLinkArgs := #["-L./.lake/packages/LeanCopilot/.lake/build/lib", "-lctranslate2"]
 }
+
+require checkdecls from git "https://github.com/PatrickMassot/checkdecls.git"
+
+meta if get_config? env = some "dev" then
+require «doc-gen4» from git
+  "https://github.com/leanprover/doc-gen4" @ "main"
